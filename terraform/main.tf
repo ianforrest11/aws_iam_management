9e138c6d4ee96ffcb65d3ec443260d6c2f248b58
Ian Forrest <ianforrest@Ians-MacBook-Air.local>
2024-06-10 21:22:50 -0600

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,3 @@
-module "iam_user_policies" {
-  source = "git@github.com:ianforrest11/terraform_templates.git//aws/iam_user_policies?ref=main"
-}
-
 module "iam_user_creation" {
   source = "git@github.com:ianforrest11/terraform_templates.git//aws/iam_user?ref=main"
   users = var.users
